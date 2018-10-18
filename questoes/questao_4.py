28d50720211804a2b940ec197060520062301afc
JonathasBarreto <42970380+JonathasBarreto@users.noreply.github.com>
2018-10-18 15:01:51 -0300

    Update questao_4.py

--- questoes/questao_4.py
+++ questoes/questao_4.py
@@ -20,7 +20,52 @@
 ##
 def main():
     print("questao 4")
-
+    data = input("").lstrip(' ')
+    a = data[:-6]
+    b = data[-2:]
+    c = data[-5:-3]
+    ano = int(a)
+    mes = int(c)
+    dia = int(b)
+    dia = dia + 1
+    i = 0
+    dm = 0
+    if mes == 1:
+        dm = 31
+    elif mes == 2:
+        dm = 26
+    elif mes == 3:
+        dm = 31
+    elif mes == 4:
+        dm = 30
+    elif mes == 5:
+        dm = 31
+    elif mes == 6:
+        dm = 30
+    elif mes == 7:
+        dm = 31
+    elif mes == 8:
+        dm = 31
+    elif mes == 9:
+        dm = 30
+    elif mes == 10:
+        dm = 31
+    elif mes == 11:
+        dm = 30
+    elif mes == 12:
+        dm = 31
+    else:
+        print("mês invalido")
+    if dia > 0 and dia <= (dm + 1):
+        if dia == (dm + 1):
+            mes+=1
+            dia = 1
+            if mes == 13:
+                ano+=1
+                mes = 1
+    else:
+        print("dia do mês invalido")
+    print(ano,"-",'{0:0>2}'.format(mes),"-",'{0:0>2}'.format(dia))
 
     
 if __name__ == '__main__':
